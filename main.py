33f9c3db1d09dec4876e0507f4cfb2d80f7027f7
luiz-rocha94 <rocha.luizgustavo94@gmail.com>
2023-04-24 16:12:15 -0300

    busca para vários modelos

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ Created on Mon Apr 24 13:55:45 2023
 """
 
 import pandas as pd
-from sklearn import linear_model
+import numpy as np
+from sklearn import linear_model, svm
 from sklearn.model_selection import train_test_split, ShuffleSplit, GridSearchCV
 from sklearn.metrics import r2_score
 
@@ -23,20 +24,33 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
 # cross-validation.
 cv = ShuffleSplit(n_splits=5, test_size=0.3, random_state=0)
 
-# model.
-model = linear_model.LinearRegression()
-parameters = {'positive':[True, False]}
+# models.
+models = {'linear':linear_model.LinearRegression, 'ridge':linear_model.Ridge, 
+          'lasso':linear_model.Lasso, 'elasticnet':linear_model.ElasticNet, 
+          'svm':svm.SVR}
+params = {'linear':{}, 
+          'ridge':{'alpha':np.arange(0.1,1,0.1)}, 
+          'lasso':{'alpha':np.arange(0.1,1,0.1)}, 
+          'elasticnet':{'alpha':np.arange(0.1,1,0.1),'l1_ratio':np.arange(0.1,1,0.1)}, 
+          'svm':{'C':np.arange(0.1,1,0.1),'epsilon':np.arange(0.1,1,0.1)}}
+scores = {'linear':0, 'ridge':0, 
+          'lasso':0, 'elasticnet':0, 
+          'svm':0}
 
-# grid search.
-scoring = ['neg_mean_squared_error', 'neg_root_mean_squared_error', 'r2']
-clf = GridSearchCV(model, parameters, scoring=scoring, cv=cv, refit='r2')
-clf.fit(X_train, y_train)
-best_paramns = clf.best_params_
+for key in models.keys():
+    # model.
+    model = models[key]()
+    parameters = params[key]
 
-# final model
-model = linear_model.LinearRegression(**best_paramns)
-model.fit(X_train, y_train)
-y_test_hat = model.predict(X_test)
-score = r2_score(y_test, y_test_hat)
-
-
+    # grid search.
+    scoring = ['neg_mean_squared_error', 'neg_root_mean_squared_error', 'r2']
+    clf = GridSearchCV(model, parameters, scoring=scoring, cv=cv, refit='r2')
+    clf.fit(X_train, y_train)
+    best_paramns = clf.best_params_
+    
+    # final model
+    model = models[key](**best_paramns)
+    model.fit(X_train, y_train)
+    y_test_hat = model.predict(X_test)
+    score = r2_score(y_test, y_test_hat)
+    scores[key] = score
